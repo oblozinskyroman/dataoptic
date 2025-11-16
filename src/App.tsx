7acb7d3b5b07f0d8694dc075ca0c3ccb92568e79
oblozinskyroman <oblozinskyroman8@gmail.com>
2025-11-17 00:47:40 +0100

    init dataoptic landing

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>DataOptic</h1>
+      <p>🚀 Moderná data intelligence & automatizácia procesov</p>
+    </main>
+  );
+}
+
+export default App;
